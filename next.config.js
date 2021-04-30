287cbaa8f6de8103eef971d5f559daa7c1ac7c1a
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2021-05-01 02:31:50 +0700

    adding config for deploment

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    trailingSlash: true,
+    exportPathMap: async function () {
+        const { projects } = data;
+        const paths = {
+            '/': { page: '/' },
+        };
+
+        projects.forEach((project) => {
+            paths[`/project/${project.slug}`] = {
+                page: '/project/[path]',
+                query: { path: project.slug },
+            };
+        });
+
+        return paths;
+    },
+    images: {
+        loader: "imgix",
+        path: "",
+    }
+};
